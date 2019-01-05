8ac811d44aa10a2864a91c655dc6630a6634a1c5
zaxcs <zaxcs@users.noreply.github.com>
2019-01-05 18:36:49 +0100

    add FastVehicles and ShowUnlock

---
+++ FasterVehicles/FasterVehicles.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Harmony12;
+using UnityModManagerNet;
+
+namespace FasterVehicles
+{
+    internal static class FasterVehicles
+    {
+        private static bool enabled;
+
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            enabled = modEntry.Active;
+            modEntry.OnToggle = (entry, state) =>
+            {
+                enabled = state;
+                return true;
+            };
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            return true;
+        }
+
+        [HarmonyPatch(typeof(PlayerScript), "updateCharacterControllerParameters")]
+        public class Patch_PlayerScript_updateCharacterControllerParameters
+        {
+            public static void Postfix(PlayerScript __instance, MilkThirdPersonController ___controller)
+            {
+                if (!enabled || !__instance.UsingVehicle)
+                    return;
+
+                ___controller.BaseSpeed += 2f;
+                ___controller.SprintSpeed += 4f;
+            }
+        }
+    }
+}
---
+++ ShowUnlock/ShowUnlock.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Reflection;
+using Harmony12;
+using Milkstone.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityModManagerNet;
+
+namespace ShowUnlock
+{
+    internal static class ShowUnlock
+    {
+        private static UnityModManager.ModEntry.ModLogger logger;
+
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            logger = modEntry.Logger;
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            return true;
+        }
+
+        [HarmonyPatch(typeof(BaseShopHud), "SetupShopButton")]
+        public class Patch_BaseShopHud_SetupShopButton
+        {
+            public static void Postfix(GameObject buttonInstance,
+                BaseShopHud.NotificationIcon notification = BaseShopHud.NotificationIcon.None)
+            {
+                try
+                {
+                    if ((int) notification != 3) return;
+
+                    var btn = buttonInstance.GetComponentInChildWithSuffix<Image>("IconNew", false);
+                    btn.sprite = SpriteLibrary.GetSprite("Star");
+                }
+                catch (Exception e)
+                {
+                    logger.Error("setup failed " + e);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(FarmShopHud), "GetButtonData")]
+        public class Patch_FarmShopHud_GetButtonData
+        {
+            public static void Postfix(ShopItemDefinition item, ref BaseShopHud.NotificationIcon notification)
+            {
+                try
+                {
+                    if (notification != BaseShopHud.NotificationIcon.None || !(item is HarvestItemDefinition) ||
+                        !needed((HarvestItemDefinition) item))
+                        return;
+
+                    notification = (BaseShopHud.NotificationIcon) 3;
+                }
+                catch (Exception e)
+                {
+                    logger.Error("get failed " + e);
+                }
+            }
+
+            private static bool needed(HarvestItemDefinition item)
+            {
+                var itemLevel = StageScript.Instance.FarmData.GetItemLevel(item);
+
+                for (var i = 0; i < (int) ShopItemType.Count; i++)
+                {
+                    var list = ShopManager.GetFarmItemList((ShopItemType) i);
+                    if (list == null)
+                        continue;
+
+                    foreach (var def in list)
+                    {
+                        if (def.ItemLevelRequirements == null || def.ItemLevelRequirements.Count == 0)
+                            continue;
+
+                        foreach (var req in def.ItemLevelRequirements)
+                        {
+                            if (!req.Item.FullId.Equals(item.FullId))
+                                continue;
+                            if (req.Level > itemLevel)
+                                return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+}
